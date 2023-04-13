ba5b8114013e4eab6ab9f8d0e526340e6377b7ab
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2023-04-13 13:09:14 +0200

    Update Hitbox.cs

--- Damage System/Hitbox.cs
+++ Damage System/Hitbox.cs
@@ -22,19 +22,7 @@ namespace WeaponSystem
 
         private void Die(DamageInstance damInst)
         {
-            var rb = GetComponent<Rigidbody>();
-            rb.isKinematic = false;
-            rb.AddForce(100f * (1 / damInst.direction.magnitude) * damInst.direction, ForceMode.Impulse);
-
             Destroy(gameObject, 2.0f);
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            var rb = gameObject.AddComponent<Rigidbody>();
-            rb.mass = 5f * ((transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3f);
-            rb.isKinematic = true;
         }
     }
 }
