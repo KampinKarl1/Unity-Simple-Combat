6eb2ccbe87801595010de56a42f8f0037443cdc6
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2023-04-13 11:16:39 +0200

    Update WeaponUser.cs

--- Weapon System/WeaponUser.cs
+++ Weapon System/WeaponUser.cs
@@ -6,18 +6,40 @@ namespace WeaponSystem
 {
     public class WeaponUser : MonoBehaviour
     {
+      
+        [SerializeField] private Transform[] transformsWithWeapons = new Transform[0];
+        private IWeapon[] weapons = new IWeapon[0];
         private IWeapon currentWeapon = null;
 
         // Start is called before the first frame update
         void Start()
         {
-            if (!TryGetComponent(out currentWeapon))
+            var tempWeaps = new List<IWeapon>();
+
+            for (int i = 0; i < transformsWithWeapons.Length; i++)
+            {
+                if (transformsWithWeapons[i].TryGetComponent(out IWeapon weapon))
+                    tempWeaps.Add(weapon);
+            }
+
+            weapons = tempWeaps.ToArray();
+
+            if (weapons == null || weapons.Length == 0)
                 print("No current weapon");
+
+            else
+                currentWeapon = weapons[0];
         }
 
         // Update is called once per frame
         void Update()
         {
+            for (int i = 0; i < weapons.Length; i++)
+            {
+                if (Input.GetKeyDown((KeyCode)i + 48))
+                    currentWeapon = weapons[i];
+            }
+
             if (Input.GetMouseButtonDown(0) && currentWeapon != null)
                 currentWeapon.UseWeapon();
         }
