a7ece32279a71250d3f9e3e8022472ab99c52bcd
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2023-04-16 21:42:26 +0200

    Put in COMMENTED Entity code in case you want to use it.

--- Damage System/Hitbox.cs
+++ Damage System/Hitbox.cs
@@ -6,11 +6,26 @@ namespace WeaponSystem
     public class Hitbox : MonoBehaviour, IDamageable
     {
         [SerializeField] private float health = 100f;
-
+        
+        //If you were to use Entities, this is where it would go
+        //[SerializeField] private Entity mainBody = null; //Allowed to be null so if this is like a red barrel or something, all it has to do is die and explode.
+        //[SerializeField] private float damageMultiplier = 1.0f;
+        
         public bool Alive() => health > 0;
 
         public void TakeDamage(DamageInstance damInst)
         {
+            //Entity stuff if you're so inclined to use it
+            /*
+                if (mainBody != null)
+                {
+                    damInst.amount *= damageMultiplier;
+                    mainBody.TakeDamage( damInst);
+                
+                    return; //DONT DO THE STUFF AFTER HERE
+                }
+            */
+        
             if (health <= 0)
                 return;
 
